a391dfaad426e4d0a3acca3bb4f59c18b91d404e
0verflowme <priyanshukumarpu@gmail.com>
2022-11-16 16:34:16 +0530

    added String Decompression

--- String Compression/Solution.java
+++ String Compression/Solution.java
@@ -1,5 +1,5 @@
 public class Solution {
-	public static String getCompressedString(String str) {
+    public static String StringCompression(String str) {
         StringBuffer sb = new StringBuffer();
         int count = 1;
         for (int i = 0; i < str.length(); i++) {
@@ -15,7 +15,19 @@ public class Solution {
         }
         return sb.toString();
     }
+
+    public static String StringDecompression(String str){
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < str.length(); i += 2) {
+            char c = str.charAt(i);
+            int times = Integer.parseInt("" + str.charAt(i + 1));
+            for (int j = 0; j < times; j++) {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
     public static void main(String[] args) {
-        System.out.println(getCompressedString("aabcccccaaa"));
+        System.out.println(StringDecompression("a2b1c5a3"));
     }
 }
